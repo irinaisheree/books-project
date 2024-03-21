8a5250dd1ad615e6438d660a21028a0dde1411a4
Irina <134360973+irinaisheree@users.noreply.github.com>
2024-03-21 16:39:08 +0200

    Add files via upload

---
+++ controllers/bookController.js
@@ -0,0 +1,57 @@
+const router = require("express").Router()
+const bookManager = require("../managers/bookManager")
+
+
+router.get('/books', async (req, res) => {
+    try {
+        const allBooks = await bookManager.getAll();
+        console.log(allBooks)
+        res.json(allBooks);
+    } catch (error) {
+        console.error('Error fetching books:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+
+router.get('/books/:bookId', async (req, res) => {
+    const bookId = req.params.bookId;
+    console.log('Requested book ID:', bookId);
+    try {
+        const oneBook = await bookManager.getOne(bookId); 
+        console.log(oneBook);
+        if (!oneBook) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        return res.json(oneBook);
+    } catch (error) {
+        console.error('Error fetching one book:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.get('/add', async (req, res) => {
+    try {
+        const book = await bookManager.getOne();
+        console.log(book)
+    } catch (error) {
+        console.error('Error fetching books:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+router.post('/add', async (req, res) => {
+    console.log(req.body)
+    try {
+        const { title, author, price, imageUrl, description } = req.body;
+        const book = await bookManager.add(title, author, price, imageUrl, description);
+        res.json({ book });
+    } catch (error) {
+        console.error('Error adding book:', error);
+        res.status(401).json({ error: 'invalid data' });
+    }
+});
+
+module.exports = router
